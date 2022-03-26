628fb925d046e9f8d612454970aec3338d7edb7d
Enes AGIR <enes.agir.7@gmail.com>
2022-03-26 13:11:24 +0100

    numbers

---
+++ 3-numbers.py
@@ -0,0 +1,21 @@
+from math import *
+
+number = 79
+number1 = 77
+number2:str
+number2 = str(number1)
+
+print(number)
+print(78+22.908)        # toplama
+print(78-22.908)        # cikartma
+print(78/22.908)        # bolme
+print(78*22.908)        # carpma
+print(78%22)            # kalansiz bolme
+print(number2)      
+print('number is ' + number2)
+print(abs(-7))          # pozitif gosterme
+print(max(7,9 ,11, 1))  # max sayiyi bulma
+print(min(7,9 ,11, 1))  # min sayiyi bulma
+print(round(4.5))       # sayiyi yuvarlamak
+print(bin(76))          # particular numaralari binary numaralara cevirmek
+print(sqrt(100))        # karakokunu almak
