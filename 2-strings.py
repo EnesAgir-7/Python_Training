56a96eeb0ed97af138783e88d3313935d71c8abb
Enes AGIR <enes.agir.7@gmail.com>
2022-03-26 12:31:29 +0100

    strings

---
+++ 2-strings.py
@@ -0,0 +1,13 @@
+name='Pasha'
+isim='ENES'
+
+print('hi \nHow are you')
+print(name[3])
+print(name.upper())
+print(name.lower())
+print(isim.isupper())
+print(isim.islower())
+print(isim.lower().islower())
+print(len(isim))
+print(isim.index('N'))
+print(isim.replace('N','Y'))
