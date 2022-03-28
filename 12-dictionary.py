452e6354d7ea6c7c7710b3bcaa65895b7ffae551
Enes AGIR <enes.agir.7@gmail.com>
2022-03-28 20:21:18 +0200

    Create 12-dictionary.py

---
+++ 12-dictionary.py
@@ -0,0 +1,19 @@
+my_dict = {
+    #^ 'Key' : 'value' 
+            #& => 2 ayni key ile farkli value giremeyiz her bir key 1 tane olmali 
+    'name': 'Pasha',
+    'nationality': 'Turkish',
+    'age': 27,
+    'is_tall': True,
+    'Qualification': 'College',
+    'friends':['Ali','Veli','Yahya']
+}
+x = my_dict['name']
+
+print(type(my_dict))
+print(my_dict)
+print(my_dict['name'])
+print(len(my_dict))
+print(my_dict['is_tall'])
+print(my_dict['friends'])
+print(x)
