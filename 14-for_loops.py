e3ac49f1364d17923148b350b6c025601b8fc6eb
Enes AGIR <enes.agir.7@gmail.com>
2022-03-29 09:07:22 +0200

    Update 14-for_loops.py

--- 14-for_loops.py
+++ 14-for_loops.py
@@ -1,2 +1,23 @@
 for letter in 'Hello':
     print(letter)
+
+#! -------------------------
+mylist = ['ji','ju','jo']
+
+for values in mylist:
+    print(values)
+
+#! -------------------------
+my_list = {
+    'name': 'Pasha',
+    'age': 16,
+}
+
+for values in my_list:
+    print(values)
+
+#! -------------------------
+for x in range(4,7):
+    print(x)
+else:
+    print('Finnish Looping!')
